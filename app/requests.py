79594cd641667b8628e767c29d569464b428c082
stephane-evrard <stephanie.evard@student.moringaschool.com>
2021-08-09 07:52:51 +0300

    function to configure requests

--- app/requests.py
+++ app/requests.py
@@ -10,3 +10,9 @@ base_url = None
 
 #getting the articles url
 articles_url = None
+
+def configure_request(app):
+    global api_key,base_url,articles_url
+	api_key = app.config['NEWS_API_KEY']
+	base_url = app.config['NEWS_SOURCES_BASE_URL']
+	articles_url = app.config['ARTICLES_BASE_URL']
